aa2659ffec8f98604824c7638af70b00b76ac1e1
NMRayhan <nurmdrayhan97@gmail.com>
2022-02-17 01:02:59 +0600

    input auto clear after error or calculation

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,19 +6,33 @@ const clothes = document.getElementById("clothes-input");
 let Expense = document.getElementById("total-expense");
 let balanceBeforeSaving = document.getElementById("first-balance");
 
-// warning msg showing 
-var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+// warning msg showing
+var alertPlaceholder = document.getElementById("liveAlertPlaceholder");
 
 function alert(message) {
-  var wrapper = document.createElement('div')
-  wrapper.innerHTML = '<div class="alert alert-danger alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
-  alertPlaceholder.appendChild(wrapper)
+  var wrapper = document.createElement("div");
+  wrapper.innerHTML =
+    '<div class="alert alert-danger alert-dismissible" role="alert">' +
+    message +
+    '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+  alertPlaceholder.appendChild(wrapper);
+}
+
+//input field auto clear
+function inputAutoClear() {
+  income.value = "";
+  food.value = "";
+  rent.value = "";
+  clothes.value = "";
+  Expense.innerText = '0.0';
+  balanceBeforeSaving.innerText = '0.0';
 }
 
 function stringToFloat(number) {
   const result = parseFloat(number);
   if (result < 0) {
-    alert("You don't input any Negative number")
+    alert("You don't input any Negative number");
+    inputAutoClear();
   } else {
     return result;
   }
@@ -32,21 +46,33 @@ document.getElementById("calc-btn").addEventListener("click", function () {
   const clothesCost = clothes.value;
 
   if (
-    (incomeCost == null || incomeCost == "") ||
-    (foodCost == null || foodCost == "") ||
-    (rentCost == null || rentCost == "") ||
-    (clothesCost == null || clothesCost == "")
+    incomeCost == null ||
+    incomeCost == "" ||
+    foodCost == null ||
+    foodCost == "" ||
+    rentCost == null ||
+    rentCost == "" ||
+    clothesCost == null ||
+    clothesCost == ""
   ) {
     alert("Please fillup every input fild");
+    inputAutoClear();
   } else {
-      let income = stringToFloat(incomeCost);
-      let food = stringToFloat(foodCost);
-      let rent = stringToFloat(rentCost);
-      let clothes = stringToFloat(clothesCost);
+    let income = stringToFloat(incomeCost);
+    let food = stringToFloat(foodCost);
+    let rent = stringToFloat(rentCost);
+    let clothes = stringToFloat(clothesCost);
     const totalExpense = food + rent + clothes;
     const remainAmount = income - totalExpense;
+
+    if (totalExpense >= remainAmount) {
+      alert("Your Expense is More then income amount");
+      inputAutoClear();
+    } else {
+      Expense.innerText = totalExpense;
+      balanceBeforeSaving.innerText = remainAmount;
+    }
 
     console.log(totalExpense, remainAmount);
   }
 });
-
