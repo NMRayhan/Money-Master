ab4cd6bf9952d43bcf404013057e2c0f7f06d106
NMRayhan <nurmdrayhan97@gmail.com>
2022-02-17 02:48:13 +0600

    all error handle

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,7 +20,7 @@ function alert(message, type) {
     ' alert-dismissible" role="alert">' +
     message +
     '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
-  alertPlaceholder.appendChild(wrapper);
+  alertPlaceholder.append(wrapper);
 }
 
 //input field auto clear
@@ -29,8 +29,11 @@ function inputAutoClear() {
   food.value = "";
   rent.value = "";
   clothes.value = "";
+  document.getElementById("savingsParentage").value = "";
   Expense.innerText = "0.0";
   balanceBeforeSaving.innerText = "0.0";
+  savingPar.innerText = "0.0";
+  balanceAfterSaving.innerText = "0.0";
 }
 
 // string to float conversion
@@ -38,7 +41,7 @@ function stringToFloat(number) {
   const result = parseFloat(number);
   if (result < 0) {
     setTimeout(() => {
-      alert("You don't input any Negative number", "danger");
+      alert("Don't input any Negative number", "danger");
     }, 1000);
     inputAutoClear();
   } else {
@@ -64,7 +67,7 @@ function calculation() {
     clothesCost == ""
   ) {
     setTimeout(() => {
-      alert("Please fillup every input fild", "danger");
+      alert("Must fill-up Income and Expense field", "danger");
     }, 1000);
     inputAutoClear();
   } else {
@@ -80,12 +83,12 @@ function calculation() {
         alert("Your Expense is More then income", "danger");
       }, 1000);
       inputAutoClear();
-    } else {
-      setTimeout(() => {
-        alert("Calculation Done", "success");
-      }, 1000);
+    } else if (income > 0 && food > 0 && rent > 0 && clothes > 0) {
       Expense.innerText = totalExpense;
       balanceBeforeSaving.innerText = remainAmount;
+      setTimeout(() => {
+        alert("Calculation Done", "success");
+      }, 100);
     }
   }
 }
@@ -93,15 +96,30 @@ function calculation() {
 //savings calculation
 function savingCalc() {
   const saving = document.getElementById("savingsParentage").value;
-  const savingText = stringToFloat(saving);
+  const savingText = parseFloat(saving);
   let balance = balanceBeforeSaving.innerText;
   let balanceText = stringToFloat(balance);
 
-  let savingsAmount = (balanceText / 100) * savingText;
-  let finalBalance = balanceText - savingsAmount;
-
-  console.log(savingsAmount, finalBalance);
-
-    savingPar.innerText = savingsAmount;
-    balanceAfterSaving.innerText = finalBalance;
+  if (balanceText > 0) {
+    if (savingText < 0) {
+      setTimeout(() => {
+        alert("Don't input any Negative number", "danger");
+      }, 500);
+    } else if (saving == null || saving == "") {
+      setTimeout(() => {
+        alert("Please fill-up Savings field", "danger");
+      }, 500);
+      inputAutoClear();
+    } else {
+      let savingsAmount = (balanceText / 100) * savingText;
+      let finalBalance = balanceText - savingsAmount;
+      savingPar.innerText = savingsAmount;
+      balanceAfterSaving.innerText = finalBalance;
+    }
+  }else{
+    setTimeout(() => {
+        alert("Must fill-up Income and Expense field before Savings Calculation", "danger");
+      }, 500);
+      inputAutoClear();
+  }
 }
