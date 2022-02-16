325679394a848cfea80415badc9e260731368932
NMRayhan <nurmdrayhan97@gmail.com>
2022-02-17 02:01:53 +0600

    avings calculation done

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,13 +6,18 @@ const clothes = document.getElementById("clothes-input");
 let Expense = document.getElementById("total-expense");
 let balanceBeforeSaving = document.getElementById("first-balance");
 
+const savingPar = document.getElementById("saving-amount");
+const balanceAfterSaving = document.getElementById("second-balance");
+
 // warning msg showing
 var alertPlaceholder = document.getElementById("liveAlertPlaceholder");
 
-function alert(message) {
+function alert(message, type) {
   var wrapper = document.createElement("div");
   wrapper.innerHTML =
-    '<div class="alert alert-danger alert-dismissible" role="alert">' +
+    '<div class="alert alert-' +
+    type +
+    ' alert-dismissible" role="alert">' +
     message +
     '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
   alertPlaceholder.appendChild(wrapper);
@@ -28,11 +33,12 @@ function inputAutoClear() {
   balanceBeforeSaving.innerText = "0.0";
 }
 
+// string to float conversion
 function stringToFloat(number) {
   const result = parseFloat(number);
   if (result < 0) {
     setTimeout(() => {
-        alert("You don't input any Negative number");
+      alert("You don't input any Negative number", "danger");
     }, 1000);
     inputAutoClear();
   } else {
@@ -58,7 +64,7 @@ function calculation() {
     clothesCost == ""
   ) {
     setTimeout(() => {
-        alert("Please fillup every input fild");
+      alert("Please fillup every input fild", "danger");
     }, 1000);
     inputAutoClear();
   } else {
@@ -71,14 +77,31 @@ function calculation() {
 
     if (totalExpense >= remainAmount) {
       setTimeout(() => {
-        alert("Your Expense is More then income amount");
+        alert("Your Expense is More then income", "danger");
       }, 1000);
       inputAutoClear();
     } else {
+      setTimeout(() => {
+        alert("Calculation Done", "success");
+      }, 1000);
       Expense.innerText = totalExpense;
       balanceBeforeSaving.innerText = remainAmount;
     }
-
-    console.log(totalExpense, remainAmount);
   }
+}
+
+//savings calculation
+function savingCalc() {
+  const saving = document.getElementById("savingsParentage").value;
+  const savingText = stringToFloat(saving);
+  let balance = balanceBeforeSaving.innerText;
+  let balanceText = stringToFloat(balance);
+
+  let savingsAmount = (balanceText / 100) * savingText;
+  let finalBalance = balanceText - savingsAmount;
+
+  console.log(savingsAmount, finalBalance);
+
+    savingPar.innerText = savingsAmount;
+    balanceAfterSaving.innerText = finalBalance;
 }
